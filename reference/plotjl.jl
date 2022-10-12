08623c59a7e0fbc1188b24b5bb50d3f27f8a53ee
Ray Ausan <rausan2@illinois.edu>
2022-10-11 22:58:58 -0500

    updated plot

--- reference/plotjl.jl
+++ reference/plotjl.jl
@@ -7,8 +7,11 @@ df = CSV.read(joinpath(dirname(@__FILE__), "Concrete_Data.csv"), DataFrame)
 
 namedf = names(df)
 rename!(df, [:Cement, :BlastFurnaceSlag, :FlyAsh, :Water, :Superplasticizer, :CoarseAggregate, :FineAggregate, :Age, :Strength])
-
+12
 sdf = stack(df, Not([:Strength, :Age]); variable_name = :Component)
 
-@df sdf violin(string.(:Component), :value, linewidth=0, size=(800, 800), ylabel="Mass (kg)", legend=false)
+@df sdf violin(string.(:Component), :value, linewidth=0, size=(800, 500), ylabel="Mass (kg)", legend=false)
 @df sdf boxplot!(string.(:Component), :value, fillalpha=0.75, linewidth=2, legend=false)
+@df sdf dotplot!(string.(:Component), :value, markersize=1, markercolor=:black)
+
+savefig(joinpath(dirname(@__FILE__), "BoxViolinDot.png"))
