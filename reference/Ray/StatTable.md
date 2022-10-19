6a62a1625ba8f5ecae819b10b10cf3a7f93f8991
Ray Ausan <rausan2@illinois.edu>
2022-10-19 16:32:42 -0500

    added markdown for tables

---
+++ reference/Ray/StatTable.md
@@ -0,0 +1,11 @@
+|         variable |    mean |   min | median |    max | nmissing |  eltype |
+|------------------|---------|-------|--------|--------|----------|---------|
+|           Cement | 281.168 | 102.0 |  272.9 |  540.0 |        0 | Float64 |
+| BlastFurnaceSlag | 73.8958 |   0.0 |   22.0 |  359.4 |        0 | Float64 |
+|           FlyAsh | 54.1883 |   0.0 |    0.0 |  200.1 |        0 | Float64 |
+|            Water | 181.567 | 121.8 |  185.0 |  247.0 |        0 | Float64 |
+| Superplasticizer | 6.20466 |   0.0 |    6.4 |   32.2 |        0 | Float64 |
+|  CoarseAggregate | 972.919 | 801.0 |  968.0 | 1145.0 |        0 | Float64 |
+|    FineAggregate |  773.58 | 594.0 |  779.5 |  992.6 |        0 | Float64 |
+|              Age | 45.6621 |     1 |   28.0 |    365 |        0 |   Int64 |
+|         Strength |  35.818 |  2.33 | 34.445 |   82.6 |        0 | Float64 |
--- reference/Ray/plotjl.jl
+++ reference/Ray/plotjl.jl
@@ -2,18 +2,34 @@ using CSV
 using DataFrames
 using Plots
 using StatsPlots
+using PlotThemes
+using PrettyTables
+using StatsBase
+using Markdown
+theme(:default)
 
 df = CSV.read(joinpath(dirname(@__FILE__), "Concrete_Data.csv"), DataFrame)
 
 namedf = names(df)
 rename!(df, [:Cement, :BlastFurnaceSlag, :FlyAsh, :Water, :Superplasticizer, :CoarseAggregate, :FineAggregate, :Age, :Strength])
 stat = describe(df)
-# st = std(df)
+st = @df df std(:Age)
 
 sdf = stack(df, Not([:Strength, :Age]); variable_name = :Component)
-
 @df sdf violin(string.(:Component), :value, linewidth=0, size=(800, 800), ylabel="Mass (kg)", legend=false)
 @df sdf boxplot!(string.(:Component), :value, fillalpha=0.75, linewidth=2, legend=false)
-@df sdf dotplot!(string.(:Component), :value, markersize=1, markercolor=:black)
+@df sdf display(dotplot!(string.(:Component), :value, markersize=1, markercolor=:black))
+
+@df df violin(["Age"], :Age, linewidth=0, size=(300, 600), ylabel="Days", legend=false)
+@df df display(boxplot!(["Age"], :Age, fillalpha=0.75, linewidth=2, legend=false))
+
+@df df violin(["Strength"], :Strength, linewidth=0, size=(300, 600), ylabel="psi", legend=false)
+@df df display(boxplot!(["Strength"], :Strength, fillalpha=0.75, linewidth=2, legend=false))
+
+conf = set_pt_conf(tf = tf_markdown);
+pt = pretty_table_with_conf(conf, String, stat, nosubheader=true)
+io = open(joinpath(dirname(@__FILE__), "StatTable.md"), "w")
+write(io, pt)
+close(io)
 
 # savefig(joinpath(dirname(@__FILE__), "BoxViolinDot.png"))
