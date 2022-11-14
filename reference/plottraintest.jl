fb73718aba932bf8264fae30c73fbbd349f6eaee
Ray Ausan <rausan2@illinois.edu>
2022-11-14 13:20:42 -0600

    update random forest section

--- reference/plottraintest.jl
+++ reference/plottraintest.jl
@@ -11,8 +11,8 @@ function plottraintest(ytrain, y_pr_train, ytest, y_pr_test)
     size = (500,500),
     aspect_ratio=:equal, legend=false)
     plot!(xx, xx, st=:line)
-    mse_SVR = mse(y_pr_train, ytrain)
-    annotate!(15,75, text("mse = $(round(mse_SVR, sigdigits=2))", 10))
+    rmse_train = sqrt(mse(y_pr_train, ytrain))
+    annotate!(15,75, text("rmse = $(round(rmse_train, sigdigits=3))", 10))
 
     compare_plt_test = plot(ytest, y_pr_test, 
     st=:scatter,
@@ -23,8 +23,8 @@ function plottraintest(ytrain, y_pr_train, ytest, y_pr_test)
     size = (500,500),
     aspect_ratio=:equal, legend=false)
     plot!(xx, xx, st=:line)
-    mse_SVR = mse(y_pr_test, ytest)
-    annotate!(15,75, text("mse = $(round(mse_SVR, sigdigits=2))", 10))
+    rmse_test = sqrt(mse(y_pr_test, ytest))
+    annotate!(15,75, text("rmse = $(round(rmse_test, sigdigits=3))", 10))
 
     title1 = ["($i)" for j in 1:1, i in 1:2]
     plt2 = plot(compare_plt_train, compare_plt_test, dpi=300, size=(700, 400), left_margin = [1mm 0mm], bottom_margin = [0mm 0mm], guidefontsize = fsize, xtickfont = font(ftsize), ytickfont = font(ftsize), title=title1, titleloc = :right, titlefont = font(fsize))
